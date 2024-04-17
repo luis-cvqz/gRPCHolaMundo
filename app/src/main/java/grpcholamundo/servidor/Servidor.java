195665898f7d2238dfa75b87372d2281b11663b5
Luis C <zs21013875@estudiantes.uv.mx>
2024-04-16 21:23:11 -0600

    Initial commit

---
+++ app/src/main/java/grpcholamundo/servidor/Servidor.java
@@ -0,0 +1,29 @@
+package grpcholamundo.servidor;
+
+import java.io.IOException;
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
+
+public class Servidor {
+    public static void main(String[] args) throws IOException, InterruptedException {
+        int puerto = 7887;
+
+        Server servidor = ServerBuilder.forPort(puerto).addService(new ServidorImpl()).build();
+
+        servidor.start();
+
+        System.out.println("Servidor iniciado...");
+        System.out.println("Escuchando en el puerto: " + puerto);
+
+        Runtime.getRuntime().addShutdownHook(new Thread() {
+            @Override
+            public void run() {
+                System.out.println("Recibiendo solicitud de apagado...");
+                servidor.shutdown();
+                System.out.println("Servidor detenido.");
+            }
+        });
+
+        servidor.awaitTermination();
+    }
+}
---
+++ app/src/main/java/grpcholamundo/servidor/ServidorImpl.java
@@ -0,0 +1,18 @@
+package grpcholamundo.servidor;
+
+import com.proto.saludo.Holamundo.SaludoRequest;
+import com.proto.saludo.Holamundo.SaludoResponse;
+import com.proto.saludo.SaludoServiceGrpc;
+
+import io.grpc.stub.StreamObserver;
+
+public class ServidorImpl extends SaludoServiceGrpc.SaludoServiceImplBase {
+    @Override
+    public void saludo(SaludoRequest request, StreamObserver<SaludoResponse> responseObserver) {
+        SaludoResponse respuesta = SaludoResponse.newBuilder().setResultado("Hola" + request.getNombre()).build();
+        
+        responseObserver.onNext(respuesta);
+
+        responseObserver.onCompleted();
+    }
+}
