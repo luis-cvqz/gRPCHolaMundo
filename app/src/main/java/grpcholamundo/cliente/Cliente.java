21391d8e44ba2462f04635dbe64a105cdf01c390
Luis C <zs21013875@estudiantes.uv.mx>
2024-04-16 21:32:33 -0600

    Client implementation

---
+++ app/src/main/java/grpcholamundo/cliente/Cliente.java
@@ -0,0 +1,25 @@
+package grpcholamundo.cliente;
+
+import com.proto.saludo.SaludoServiceGrpc;
+import com.proto.saludo.Holamundo.SaludoRequest;
+import com.proto.saludo.Holamundo.SaludoResponse;
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+
+public class Cliente {
+    public static void main(String[] args) {
+        String host = "localhost";
+        int puerto = 8080;
+
+        ManagedChannel channel = ManagedChannelBuilder.forAddress(host, puerto).usePlaintext().build();
+
+        SaludoServiceGrpc.SaludoServiceBlockingStub stub = SaludoServiceGrpc.newBlockingStub(channel);
+        SaludoRequest peticion = SaludoRequest.newBuilder().setNombre("Luis").build();
+        SaludoResponse respuesta = stub.saludo(peticion);
+
+        System.out.println("Respuesta RPC: " + respuesta.getResultado());
+
+        System.out.println("Apagando...");
+        channel.shutdown();
+    }
+}
